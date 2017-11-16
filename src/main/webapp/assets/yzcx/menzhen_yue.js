90ebd5b06307d1b8952ec1fc5b635ece2b3dc928
liudongguang <liudongguang@qq.com>
2017-11-16 19:24:27 +0800

    20171116 门诊信息

--- src/main/webapp/assets/yzcx/menzhen_yue.js
+++ src/main/webapp/assets/yzcx/menzhen_yue.js
@@ -3,10 +3,17 @@ $(document).ready(function() {
         years: [2017],
         topOffset: 6,
         onMonthSelect: function(m, y) {
-            var d = new Date()
-           console.log(d)
+            var d1 = new Date()
+            var d2=new Date(y,(m+1),1);
+            //选择的日期要小于当前日期
+            if(d1>d2){
+
+            }else{
+                alert("必须选择前一个月日期！");
+            }
         }
     })
+
     Highcharts.setOptions({ global: { useUTC: false } });
     ajaxRequest("webyzcx/menzhen_yueChart", null, function (data) {
         $('#container').highcharts(data.menzhen);
