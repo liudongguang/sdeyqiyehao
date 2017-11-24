94d8cce85522b579a36e0cb710797c49efcfd666
liudongguang <liudongguang@qq.com>
2017-11-24 15:13:05 +0800

    20171124 费用图表展示

--- src/main/webapp/assets/yzcx/feiyong/feiyong_yue.js
+++ src/main/webapp/assets/yzcx/feiyong/feiyong_yue.js
@@ -4,17 +4,19 @@ $(document).ready(function() {
         topOffset: 6,
         onMonthSelect: function(m, y) {
             var d1 = new Date()
-            var d2=new Date(y,(m+1),1);
+            var d2=new Date(y,m,1);
             //选择的日期要小于当前日期
             if(d1>d2){
                  var month=d2.getMonth().toString();
-                 if(month.length==1){
+                 month=parseInt(month)+1;
+                 if(month.toString().length==1){
                      month="0"+month;
                  }
                  var date=d2.getFullYear()+"-"+month+"-01";
-                location.href="webyzcxFeiYong/feiyong_yue_page?start="+date;
+                 var url="webyzcxFeiYong/feiyong_yue_page?start="+date;
+                 location.href=url;
             }else{
-                alert("必须选择前一个月日期！");
+                alert("不能大于本月！");
             }
         }
     })
