f5176634d4667fff5e988be20234a8d946aa0129
liudongguang <liudongguang@qq.com>
2018-01-04 09:30:07 +0800

    20180104

--- src/main/java/com/yzcx/controller/YZCXHandlerController.java
+++ src/main/java/com/yzcx/controller/YZCXHandlerController.java
@@ -205,4 +205,6 @@ public class YZCXHandlerController {
     public void sendBirthday() throws IOException, ParseException {
 
     }
+
+
 }
--- src/test/java/com/date/DateTest.java
+++ src/test/java/com/date/DateTest.java
@@ -1,10 +1,12 @@
 package com.date;
 
 import com.yzcx.api.util.LdgDateUtil;
+import com.yzcx.api.vo.YZCXSearchParam;
 import org.junit.Test;
 
 import java.text.ParseException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class DateTest {
     @Test
@@ -20,5 +22,11 @@ public class DateTest {
         String nowDateTime_Str= nowTime.format(LdgDateUtil.newDateFormat_yyyy_mm_dd_HH_mm_ss);
         System.out.println(nowDateTime_Str);
     }
+    @Test
+    public void t3() throws ParseException {
+        List<YZCXSearchParam> initDateList=  LdgDateUtil.getStartAndEndTimeByTiQianYueNum(1);
+        System.out.println(initDateList);
+    }
+
 
 }
